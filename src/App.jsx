94e9bb8c203c70c85acb3b3242b02e9ad37e9066
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-01-16 20:00:50 -0300

    aula-02: componentes iniciais e fontes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { styled } from 'styled-components'
 import GlobalStyles from './components/GlobalStyles'
+import Cabecalho from './components/Header'
+import CampoTexto from './components/CampoTexto'
+import BarraLateral from './components/BarraLateral'
+import Banner from './components/Banner'
+import bannerImage from './assets/banner.png' 
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -12,6 +17,14 @@ function App() {
   return (
     <FundoGradiente>
       <GlobalStyles />
+      <Cabecalho>
+        <CampoTexto type="search"/>
+      </Cabecalho>
+      <BarraLateral />
+      <Banner 
+        texto="A galeria mais completa de fotos do espaço!"
+        backgroundImage={bannerImage}
+      />
     </FundoGradiente>
   )
 }
